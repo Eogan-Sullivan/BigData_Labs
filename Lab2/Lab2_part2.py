c746bcda75889e2a59ac3dbbe0c9c37fe48ada0a
eogan-sullivan <eogan.sullivan@outlook.com>
2018-01-31 12:55:20 +0000

    Lab2 contd

--- Lab2/Lab2_part2.py
+++ Lab2/Lab2_part2.py
@@ -6,7 +6,7 @@ Created on Wed Jan 31 10:05:45 2018
 """
 import pandas as pd
 import numpy as np
-data = pd.read_csv('C:/Users/t00166087/Desktop/Lab2/lab2_datafiles/Customer Churn Model.txt')
+data = pd.read_csv('C:/Users/t00166087/Desktop/BigData_Labs/Lab2/lab2_datafiles/Customer Churn Model.txt')
 column_list = data.columns.values.tolist()
 
 np.random.choice(column_list)
@@ -56,3 +56,98 @@ def pi_run(nums,loops):
     return(pi,fig)
     
 pi_run(1000,100)
+
+d=pd.DataFrame({'A':np.random.randn(10),'B':2.5*np.random.randn(10)+1.5})
+d
+
+column_list = data.columns.values.tolist()
+a=len(column_list)
+d=pd.DataFrame({'Column_Name':column_list,'A':np.random.rand(a),'B':2.5*np.random.randn(a)+1.5})
+d
+
+d=pd.DataFrame({'A':np.random.randn(10),'B':2.5*np.random.randn(10)+1.5},index=range(10,20))
+d
+
+a=['Male','Female']
+b =['Rich','Poor','Middle Class']
+gender = []
+seb =[]
+for i in range(1,101):
+    gender.append(np.random.choice(a))
+    seb.append(np.random.choice(b))
+height=30*np.random.randn(100)+155
+weight=20*np.random.randn(100)+60
+age = 10*np.random.randn(100)+35
+income=1500*np.random.randn(100)+15000
+
+df=pd.DataFrame({'Gender':gender,'Height':height,'Weight':weight,'Age':age,'Income':income,'Socio-Eco':seb})
+df.head
+
+df.groupby('Gender')
+grouped =df.groupby('Gender')
+grouped.groups
+
+grouped = df.groupby('Gender')
+for names,groups in grouped:
+    print(names)
+    print(groups)
+    
+grouped.get_group('Female')
+
+grouped= df.groupby(['Gender','Socio-Eco'])
+for names,groups in grouped:
+    print(names)
+    print(groups)
+    
+len(grouped)
+
+grouped= df.groupby(['Gender','Socio-Eco'])
+grouped.sum()
+grouped.size()
+grouped.describe()
+
+grouped_income = grouped['Income']
+
+grouped.aggregate({'Income':np.sum,'Age':np.mean,'Height':np.std})
+
+grouped.aggregate({'Age':np.mean,'Height':lambda x:np.mean(x)/np.std(x)})
+
+grouped.aggregate([np.sum,np.mean,np.std])
+
+grouped['Age'].filter(lambda x:x.sum()>700)
+
+zscore = lambda x:(x-x.mean()/x.std())
+grouped.transform(zscore)
+
+f=lambda x:x.fillna(x.mean())
+grouped.transform(f)
+
+grouped.head(1)
+
+grouped.tail(1)
+
+grouped=df.groupby('Gender')
+grouped.nth(1)
+
+df1 =df.sort_values(['Age','Income'])
+grouped = df1.groupby('Gender')
+grouped.head(1)
+grouped.tail(1)
+
+len(data)
+a=np.random.randn(len(data))
+check=a<0.8
+training = data[check]
+testing = data[~check]
+len(training)
+len(testing)
+
+from sklearn.cross_validation import train_test_split
+train,test=train_test_split(data,test_size = 0.2)
+
+import numpy as np
+with open(('C:/Users/t00166087/Desktop/BigData_Labs/Lab2/lab2_datafiles/Customer Churn Model.txt','rb') as f:
+    data =f.read().split('\n')
+np.random.shuffle(data)
+train_data = data[:3*len(data)/4]
+test_data = data[len(data)/4:]
